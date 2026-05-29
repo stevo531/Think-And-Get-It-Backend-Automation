225a8a76cec4f775a34702da328075fa05716ce3
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2026-05-29 15:15:32 +0200

    created branch for registration with constants&payloads added

---
+++ src/main/java/payloads/RegisterUser.java
@@ -0,0 +1,34 @@
+package payloads;
+
+public class RegisterUser {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    // Constructors
+    public RegisterUser(String email, String password, String firstName, String lastName, String phone) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    // Getters & Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+}
