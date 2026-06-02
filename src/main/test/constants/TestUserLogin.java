dfedd095871cebdc2bafda650db2ac0177ffb932
stevo531 <steven.cyubahiro24snhu@keplercollege.ac.rw>
2026-06-02 11:51:31 +0200

    created test for user registration, verified status code

---
+++ src/main/test/constants/TestUserLogin.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class TestUserLogin {
+    public static final String EMAIL = "cyubahirosteven89@gmail.com";
+    public static final String PASSWORD = "new@keplercollege@25";
+}
---
+++ src/main/test/user/AuthenticateUser.java
@@ -0,0 +1,29 @@
+package user;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import payloads.RegisterUser;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthenticateUser {
+
+    static {
+        RestAssured.baseURI =
+                "https://think-and-get-it-production.up.railway.app/api/v1";
+    }
+
+    public static Response registerUser(RegisterUser payload) {
+        return given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/auth/register")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
+}
